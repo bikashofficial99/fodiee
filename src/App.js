35caf2deaebacde05beb46696b3109f19cf8310d
Bikash Acharya <bikash.acharya614@gmail.com>
2024-09-19 23:00:10 +0545

    About rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return <div className="App">
-    <Home/>
+    <Home />
+    <About />
     </div>;
 }
 
