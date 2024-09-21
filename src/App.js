a7c6efe5534a1ee34391d58a0e4b734aa9caf7bc
Bikash Acharya <bikash.acharya614@gmail.com>
2024-09-21 23:23:47 +0545

    Testimonial Rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Work from "./components/Work";
+import Testimonial from "./components/Testimonial";
+
 function App() {
   return <div className="App">
     <Home />
     <About />
     <Work />
+    <Testimonial />
     </div>;
 }
 
