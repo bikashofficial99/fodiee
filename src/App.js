6f0410d5662a950cfedc14d5f7d6eb2a3bf705d2
Bikash Acharya <bikash.acharya614@gmail.com>
2024-09-23 18:14:35 +0545

    Footer rendering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Work from "./components/Work";
 import Testimonial from "./components/Testimonial";
 import Contact from "./components/Contact";
-
+import Footer from "./components/Footer";
 function App() {
   return <div className="App">
     <Home />
@@ -12,6 +12,7 @@ function App() {
     <Work />
     <Testimonial />
     <Contact />
+    <Footer />
     </div>;
 }
 
